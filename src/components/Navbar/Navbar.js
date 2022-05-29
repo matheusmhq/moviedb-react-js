efd827f28470d9cf945002f0b425286837a1d0a6
Matheus Henrique <mhqsoftware@gmail.com>
2022-05-29 03:09:46 -0300

    fix: remove import not used

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { InputGroup, FormControl } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import logo from "assets/images/logo.png";
 import * as S from "./styles";
